03f51eae438e8eb9da48996b763be81790cfa534
Paul Sanders <psanders1@gmail.com>
2023-06-27 10:29:12 -0400

    Remove extra files

--- server/routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const goalController = require("../controllers/goalController");
-
-router.post("/", goalController.createGoal);
-router.get("/", goalController.getGoals);
-router.put("/:id", goalController.updateGoal);
-
-module.exports = router;
