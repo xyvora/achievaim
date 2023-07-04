cd6606fae3f129c75488fc62b8ab7ccf12ddb92e
coaleyyy <112022464+coaleyyy@users.noreply.github.com>
2023-07-04 13:41:45 -0400

    all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,13 @@ export default defineConfig({
   plugins: [sveltekit()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+        host: 'localhost'
+    },
   }
+  
 });
+
