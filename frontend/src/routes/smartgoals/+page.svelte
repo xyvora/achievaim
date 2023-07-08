a6605ba205a3fe02c304a47a92b4e56b73495632
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-08 01:01:42 -0400

    Updated nav bar routing
    Moved pages under folder locations for svelte routing
    
    Created temp routes for upcoming goal tracking

---
+++ frontend/src/routes/smartgoals/+page.svelte
@@ -0,0 +1,29 @@
+<div class="card flex-shrink-0 w-full lg:w-1/3 max-w-sm shadow-2xl bg-base-100">
+    <div class="card-body">
+      <div class="form-control">
+        <label class="label" for="email-input">
+          <span class="label-text">Email</span>
+        </label>
+        <input type="text" placeholder="email" id="email-input" class="input input-bordered" />
+      </div>
+  
+      <div class="form-control">
+        <label class="label" for="password-input">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="password"
+          id="password-input"
+          class="input input-bordered"
+        />
+        <a href="#" class="label-text-alt link link-hover mt-4 mb-4">Forgot password?</a>
+        <a href="#" class="label-text-alt link link-hover">Are your Goals Smart yet? Sign up here!</a>
+      </div>
+      <div class="form-control mt-6">
+        <button class="btn btn-primary">Login</button>
+      </div>
+      <div class="form-control mt-6 text-center text-lg font-bold" />
+    </div>
+  </div>
+  
