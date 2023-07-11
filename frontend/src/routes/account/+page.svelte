231bbcdc2048a779d72a368b57f25a9a6947647c
Paul Sanders <psanders1@gmail.com>
2023-07-10 22:10:16 -0400

    Add validators to account fields

--- frontend/src/routes/account/+page.svelte
+++ frontend/src/routes/account/+page.svelte
@@ -15,31 +15,33 @@
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label
         class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-first-name"
+        for="first-name"
       >
-        First Name
+        First Name*
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-        id="grid-first-name"
+        id="first-name"
         type="text"
         placeholder="Jane"
         bind:value={user.firstName}
+        required
       />
     </div>
     <div class="w-full md:w-1/2 px-3">
       <label
         class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-last-name"
+        for="last-name"
       >
-        Last Name
+        Last Name*
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-last-name"
+        id="last-name"
         type="text"
         placeholder="Doe"
         bind:value={user.lastName}
+        required
       />
     </div>
   </div>
@@ -47,28 +49,26 @@
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label
         class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-username"
+        for="username"
       >
-        Username
+        Username*
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-username"
+        id="username"
         type="text"
         placeholder="jane.doe"
         bind:value={user.username}
+        required
       />
     </div>
     <div class="w-full md:w-1/2 px-3">
-      <label
-        class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-gmail"
-      >
+      <label class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2" for="gmail">
         Gmail Account
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-gmail"
+        id="gmail"
         type="email"
         placeholder="jane.doe@gmail.com"
         bind:value={user.gmail}
@@ -79,13 +79,13 @@
     <div class="w-full px-3">
       <label
         class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-country"
+        for="country"
       >
         Country
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-country"
+        id="country"
         type="text"
         placeholder="United States"
         bind:value={user.country}
@@ -96,13 +96,13 @@
     <div class="w-full px-3">
       <label
         class="block uppercase tracking-wide text-gray-700 text-lg font-bold mb-2"
-        for="grid-avatar"
+        for="avatar"
       >
         Avatar Photo URL
       </label>
       <input
         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        id="grid-avatar"
+        id="avatar"
         type="url"
         placeholder="https://example.com/image.jpg"
         bind:value={user.avatar}
@@ -113,8 +113,15 @@
     <button
       class="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       type="submit"
+      id="btnSubmit"
     >
       Sign Up
     </button>
   </div>
 </form>
+
+<style>
+  input:invalid {
+    border: 2px solid red;
+  }
+</style>
