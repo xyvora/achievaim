ab780c6d85ef707b3828f3fb08b6eb665497db25
Paul Sanders <psanders1@gmail.com>
2023-07-13 14:37:39 -0400

    Add basic home page for API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
+from typing import Any
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 
 from app.api.deps import logger
 from app.api.v1.api import api_router
@@ -7,6 +12,7 @@ from app.db import init_db
 
 app = FastAPI()
 app.include_router(api_router)
+templates = Jinja2Templates(directory="app/templates")
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,3 +27,8 @@ app.add_middleware(
 async def startup() -> None:  # pragma: no cover
     logger.info("Initializing the database")
     await init_db()
+
+
+@app.get("/", include_in_schema=False)
+def index(request: Request) -> Any:
+    return templates.TemplateResponse("index.html", {"request": request})
