d01c00bbacdf643cddcb69a164cbaca68c60f7c3
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-14 15:00:36 -0400

    Added a + add goals on the right hand of nav bar

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -7,7 +7,7 @@
   <div class="flex-none sm:px-2 sm:mx-2">
     <ul class="menu menu-horizontal bg-base-200 rounded-box">
       <li>
-        <a href="home" class:active={$page.url.pathname === '/home'}>
+        <a href="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             class="h-5 w-5"
@@ -19,23 +19,6 @@
               stroke-linejoin="round"
               stroke-width="2"
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-            /></svg
-          >
-        </a>
-      </li>
-      <li>
-        <a href="account" class:active={$page.url.pathname === '/account'}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            ><path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
             /></svg
           >
         </a>
@@ -59,9 +42,15 @@
       </li>
     </ul>
   </div>
+  
   <div class="flex-1 px-2 mx-2" />
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1" />
     <ThemeSelector />
+    <a href="/creategoals" class:active={$page.url.pathname === '/creategoals'}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+    </a>
   </div>
 </div>
