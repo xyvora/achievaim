c4825c3b143ce4e564d0c71741d188d582f0f0e7
Paul Sanders <psanders1@gmail.com>
2023-07-16 09:35:49 -0400

    Use svelte +error for 404

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Error from '$lib/components/Error.svelte';
+  import { onMount } from 'svelte';
+
+  let loadError = false;
+
+  onMount(async () => {
+    setTimeout(() => (loadError = true), 100);
+  });
+</script>
+
+<svelte:head>
+  {#if loadError}
+    <title>404 Page not found</title>
+  {/if}
+</svelte:head>
+
+{#if loadError}
+  <Error error={'404 Page not found'} />
+{/if}
