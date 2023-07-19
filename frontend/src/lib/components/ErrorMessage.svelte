a14525e6cd422d26161df616b0356190e950a7ae
Paul Sanders <psanders1@gmail.com>
2023-07-19 16:27:02 -0400

    Handle invalid and errored logins

---
+++ frontend/src/lib/components/ErrorMessage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let errorMessageId: string;
+  export let errorMessage: string;
+  export let showError = false;
+</script>
+
+{#if showError}
+  <div class="mt-2">
+    <span id={errorMessageId} class="text-error">{errorMessage}</span>
+  </div>
+{/if}
