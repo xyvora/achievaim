a7cac5543db901cfa42f0f6fff27f795309b63ae
Paul Sanders <psanders1@gmail.com>
2023-07-20 08:51:12 -0400

    Fix links and tests

--- frontend/tests/smartgoals.spec.ts
+++ frontend/tests/smartgoals.spec.ts
@@ -1,11 +1,11 @@
 import { expect, test } from '@playwright/test';
 
 test('page loads', async ({ page }) => {
-  await page.goto('/smartgoals');
+  await page.goto('/smart-goals');
   await expect(page.locator('#navbar')).toBeVisible();
 });
 
 test('navbar link is active', async ({ page }) => {
-  await page.goto('/smartgoals');
+  await page.goto('/smart-goals');
   await expect(page.getByRole('link', { name: /smart goals/ })).toHaveClass('active');
 });
