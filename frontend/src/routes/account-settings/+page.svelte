97de9f33b62fdc7aa5df1460cf1d307f343ac8d5
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-22 12:28:00 -0400

    Created account-settings page
    Log-Out Button pops up when user is logged in

---
+++ frontend/src/routes/account-settings/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Signup from '$lib/components/signup.svelte';
+    import { isLoggedIn, accessToken } from '$lib/stores/stores';
+
+    async function logOut() {
+        accessToken.set(null);
+    }
+</script>
+
+<main>
+    <Signup />
+    {#if $isLoggedIn}
+    <div class="ml-2">
+        <button class="px-4 py-2 text-red-500 bg-transparent border border-red-500 hover:bg-red-500 hover:text-white rounded-lg focus:outline-none transition-colors duration-200 ease-in-out" on:click={() => logOut()}>Log Out</button>
+    </div>
+    {/if}
+</main>
--- frontend/src/routes/smart-goals/+page.svelte
+++ frontend/src/routes/smart-goals/+page.svelte
@@ -69,15 +69,19 @@
 </div>
 
 <style>
-  @keyframes fade-in-down {
-    0% {
-      opacity: 0;
-      transform: translate3d(0, -50%, 0);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translate3d(0, 0, 0);
-    }
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
   }
+  40% {
+    transform: translateY(-20px);
+  }
+  60% {
+    transform: translateY(-10px);
+  }
+}
+
+button {
+  animation: bounce 1s ease ;
+}
 </style>
