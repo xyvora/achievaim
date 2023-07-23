cb8a5b6671153175c0f056fd2c42b47dc3afec76
Paul Sanders <psanders1@gmail.com>
2023-07-23 19:15:45 -0400

    Show spinner while loading

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import '../app.css';
   import Navbar from '$lib/components/Navbar.svelte';
+  import { isLoading } from '$lib/stores/stores';
+  import { onMount } from 'svelte';
+
+  isLoading.set(true);
+
+  onMount(async () => {
+    isLoading.set(false);
+  });
 </script>
 
 <div class="flex flex-col h-screen">
-  <Navbar />
-  <slot />
+  {#if $isLoading}
+    <div class="flex flex-col items-center justify-center h-screen">
+      <span class="loading loading-spinner loading-lg text-primary" />
+    </div>
+  {:else}
+    <Navbar />
+    <slot />
+  {/if}
 </div>
