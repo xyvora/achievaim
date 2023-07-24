30253d1eb097b916fa16c0e6aeaf9d1002a99b3d
Paul Sanders <psanders1@gmail.com>
2023-07-24 14:01:10 -0400

    Retry localhost

--- frontend/tests/end_to_end.spec.ts
+++ frontend/tests/end_to_end.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('end to end test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000/');
+  await page.goto('http://localhost:3000/');
 
   // Test create user
   await page.getByRole('link', { name: 'Are your Goals Smart yet? Sign up here!' }).click();
-  await expect(page).toHaveURL('http://127.0.0.1:3000/signup');
+  await expect(page).toHaveURL('http://localhost:3000/signup');
   await page.locator('#first-name').click();
   await page.locator('#first-name').fill('Imma');
   await page.locator('#last-name').click();
@@ -21,12 +21,12 @@ test('end to end test', async ({ page }) => {
   await page.locator('#btn-sign-up').click();
 
   // Wait for save to finish
-  await expect(page).toHaveURL('http://127.0.0.1:3000/account-settings', { timeout: 10000 });
+  await expect(page).toHaveURL('http://localhost:3000/account-settings', { timeout: 10000 });
   await expect(page.locator('#btn-log-out')).toBeVisible();
 
   // Test log out
   await page.getByRole('button', { name: 'Log Out' }).click();
-  await expect(page).toHaveURL('http://127.0.0.1:3000/');
+  await expect(page).toHaveURL('http://localhost:3000/');
 
   // Test log in
   await page.locator('#user-name').click();
@@ -37,7 +37,7 @@ test('end to end test', async ({ page }) => {
 
   // Test user update
   await page.getByLabel('account-settings').click();
-  await expect(page).toHaveURL('http://127.0.0.1:3000/account-settings');
+  await expect(page).toHaveURL('http://localhost:3000/account-settings');
   await page.locator('#country').click();
   await page.locator('#country').fill('Italy');
   await page.locator('#password').click();
@@ -53,7 +53,7 @@ test('end to end test', async ({ page }) => {
     await dialog.accept();
   });
   await page.locator('#btn-delete').click();
-  await expect(page).toHaveURL('http://127.0.0.1:3000/');
+  await expect(page).toHaveURL('http://localhost:3000/');
 
   // Test invalid user can't log in
   await page.locator('#user-name').click();
