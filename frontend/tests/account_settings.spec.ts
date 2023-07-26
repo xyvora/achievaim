6650f90c2e4520c7165a32965f3158804f66e62a
Paul Sanders <psanders1@gmail.com>
2023-07-25 21:47:10 -0400

    Increase timeout

--- frontend/tests/account_settings.spec.ts
+++ frontend/tests/account_settings.spec.ts
@@ -8,16 +8,16 @@ test('page loads', async ({ page }) => {
 test('required fields', async ({ page }) => {
   await page.goto('/signup');
   await page.locator('#btn-sign-up').click();
-  await expect(page.locator('#first-name-error')).toBeVisible();
-  await expect(page.locator('#last-name-error')).toBeVisible();
-  await expect(page.locator('#user-name-error')).toBeVisible();
-  await expect(page.locator('#password-error')).toBeVisible();
+  await expect(page.locator('#first-name-error')).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#last-name-error')).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#user-name-error')).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#password-error')).toBeVisible({ timeout: 10000 });
 });
 
 test('not logged in buttons', async ({ page }) => {
   await page.goto('/signup');
-  await expect(page.locator('#btn-sign-up')).toBeVisible();
-  await expect(page.locator('#btn-save')).not.toBeVisible();
-  await expect(page.locator('#btn-log-out')).not.toBeVisible();
-  await expect(page.locator('#btn-delete')).not.toBeVisible();
+  await expect(page.locator('#btn-sign-up')).toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#btn-save')).not.toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#btn-log-out')).not.toBeVisible({ timeout: 10000 });
+  await expect(page.locator('#btn-delete')).not.toBeVisible({ timeout: 10000 });
 });
