b72ad60109e892a5fea6d1e2f5ca1d681967922b
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-30 11:49:06 -0400

    Updated load animation

--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -2,7 +2,24 @@
   import AccountInfoForm from '$lib/components/AccountInfoForm.svelte';
   // Any other scripts go here
 </script>
-
+<div class="page-fade-in">
 <main>
   <AccountInfoForm />
 </main>
+<style>
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  .page-fade-in {
+    animation: fadeIn 1s ease-in-out;
+  }
+</style>
+
+
+</div>
