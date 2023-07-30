9ca4e8ea59cb4cd2a7eb4819dc29b5b514010c7f
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-30 11:33:08 -0400

    Home icon on load appearance

--- frontend/src/lib/components/Navbar.svelte
+++ frontend/src/lib/components/Navbar.svelte
@@ -37,47 +37,53 @@
               href="/"
               aria-label="home"
               title="Home"
-              class={$page.url.pathname === '/home' ? 'rounded bg-white' : ''}
+              class={$page.url.pathname === '/' ? 'rounded bg-white' : ''}
               on:click={animateSVG}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="stroke-current {$page.url.pathname === '/home'
+                class="stroke-current {$page.url.pathname === '/'
                   ? 'text-primary'
                   : 'text-white'} h-5 w-5"
                 fill="none"
                 viewBox="0 0 24 24"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" /></svg
-              ></a
+                >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+              </a
             >
           </li>
         {:else}
-          <li>
-            <a
-              href="smart-goals"
-              aria-label="smart goals"
-              title=" Smart Goals"
-              class={$page.url.pathname === '/smart-goals' ? 'rounded bg-white' : ''}
-              on:click={animateSVG}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="stroke-current {$page.url.pathname === '/smart-goals'
-                  ? 'text-primary'
-                  : 'text-white'} h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
+        <li>
+          <a
+            href="/"
+            aria-label="home"
+            title="Home"
+            class={$page.url.pathname === '/' ? 'rounded bg-white' : ''}
+            on:click={animateSVG}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="stroke-current {$page.url.pathname === '/'
+                ? 'text-primary'
+                : 'text-white'} h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"
-                />
-              </svg>
-            </a>
-          </li>
+              <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </a>
+        </li>
           <li>
             <a
               href="/create-goals"
