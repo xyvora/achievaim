9324d74db7978275430b0d3b6dc976bc92851ba5
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-07-30 21:59:29 -0400

    Added Modal

--- frontend/src/routes/create-goals/+page.svelte
+++ frontend/src/routes/create-goals/+page.svelte
@@ -49,6 +49,21 @@
       console.log(error);
     }
   }
+
+  let showModal = false; // Modal state
+
+const openModal = () => {
+  showModal = true;
+};
+
+const closeModal = () => {
+  showModal = false;
+};
+
+const handleEdit = () => {
+  // Handle the edit logic here...
+  closeModal();
+};
 </script>
 
 <div class="flex">
@@ -176,7 +191,7 @@
       </figure>
     </div>
   </div>
-</div>
+
 
 <div class="mt-4 pd-4 flex flex-col items-center">
   {#if goal.days_of_week}
@@ -190,7 +205,7 @@
     Set the alert time for your SMART goals on selected days.
   </label>
   <input
-    class="shadow appearance-none border rounded w-1/2 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+    class="shadow m-2 appearance-none border rounded px-300 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
     id="goal-time"
     type="time"
     bind:value={goal.time_of_day}
@@ -199,10 +214,10 @@
 
 <div class="mt-4 flex flex-col items-center">
   <label class="block text-lg font-bold mb-2" for="goal-date">
-    Choose the Date for Completing Your SMART Goal. Usually Associated with Bigger Goals:
+    Choose the Date for Completing Your SMART Goal.
   </label>
   <input
-    class="shadow appearance-none border rounded w-1/2 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+    class="shadow m-2 appearance-none border rounded px-300 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
     id="goal-date"
     type="date"
     bind:value={goal.date_for_achievement}
@@ -212,6 +227,46 @@
 <div class="mt-4 flex flex-col items-center">
   <button class="btn btn-primary" on:click={handleSave}>Save Smart Goal</button>
 </div>
+</div>
+
+<!-- Adding the button to open the modal -->
+<div class="mt-4 flex flex-col items-center">
+  <button class="btn btn-primary" on:click={openModal}>Edit Smart Goal</button>
+</div>
+
+{#if showModal}
+<div class="fixed z-50 inset-0 overflow-y-auto">
+  <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:items-center sm:justify-center sm:pt-0 sm:pb-4">
+    <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+      <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    </div>
+    <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full sm:my-8">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+          <!-- Modal content goes here -->
+          <div>
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+              Edit Smart Goal
+            </h3>
+            <div class="mt-2">
+              <!-- Add your form fields here -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button on:click={handleEdit} class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary text-base font-medium text-white hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary sm:ml-3 sm:w-auto sm:text-sm">
+          Edit
+        </button>
+        <button on:click={closeModal} class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-50 sm:mt-0 sm:w-auto sm:text-sm">
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+{/if}
+
 
 <div class="flex">
   <div class="divider" />
