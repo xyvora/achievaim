ff4cbb7ab82b48403ebaa80f63c098d0d1043479
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2023-08-02 20:20:14 -0400

    Updated layout

--- frontend/src/lib/components/DaysOfWeekSelectorToggle.svelte
+++ frontend/src/lib/components/DaysOfWeekSelectorToggle.svelte
@@ -41,33 +41,37 @@
                   bind:checked={selectAll}
                   on:click={toggleAll}
                 />
-              </label>
-              <div class="dropdown dropdown-end">
-                <label tabindex="0" class="btn btn-circle btn-ghost btn-xs text-info m-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    class="w-4 h-4 stroke-current"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </label>
-                <div
-                  tabindex="0"
-                  class="card compact dropdown-content z-[1] bg-base-100 rounded-box w-64"
+                <label
+                  class="cursor-pointer label flex items-center md:ml-2 mt-2 md:mt-0 w-full md:w-auto justify-end"
                 >
-                  <div class="card-body">
-                    <h2 class="card-title">You needed more info?</h2>
-                    <p>Here is a description!</p>
+                  <input type="checkbox" class="toggle toggle-primary" checked />
+                  <div class="dropdown dropdown-end">
+                    <button tabindex="0" class="btn btn-circle btn-ghost btn-xs text-info m-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="w-4 h-4 stroke-current"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                      <button
+                        class="card compact dropdown-content z-[1] bg-base-100 rounded-box w-64"
+                      >
+                        <div class="card-body">
+                          <h2 class="card-title">You needed more info?</h2>
+                          <p>Here is a description!</p>
+                        </div>
+                      </button>
+                    </button>
                   </div>
-                </div>
-              </div>
+                </label></label
+              >
             </div>
           </div>
           <DaysOfWeekSelector daysOfWeek={goal.days_of_week} />
