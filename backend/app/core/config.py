036930f5de71c209e51822f837ab8396e7281994
Paul Sanders <psanders1@gmail.com>
2023-08-05 15:20:07 -0400

    Bump fastapi 0.99.1 > 0.101.0 pydantic 1.10.12 > 2.1.1

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,7 @@ import logging
 import secrets
 from typing import Final
 
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
@@ -10,20 +10,18 @@ class Settings(BaseSettings):
 
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: Final[int] = 60 * 24 * 8
-    SECRET_KEY: Final[str] = Field(secrets.token_urlsafe(32), env="SECRET_KEY")
+    SECRET_KEY: Final[str] = secrets.token_urlsafe(32)
 
-    mongo_initdb_database: str = Field("mongo_test", env="MONGO_INITDB_DATABASE")
-    mongo_initdb_root_username: str = Field("mongo", env="MONGO_INITDB_ROOT_USERNAME")
-    mongo_initdb_root_password: str = Field("mongo_password", env="MONGO_INITDB_ROOT_PASSWORD")
-    mongo_host: str = Field("mongodb://127.0.0.1", env="MONGO_HOST")
-    mongo_port: int = Field(27017, env="MONGO_PORT")
-    log_level: int = Field(logging.INFO, env="LOG_LEVEL")
-    openai_api_key: str = Field("key", env="OPENAI_API_KEY")
-    openai_organization: str | None = Field(None, env="OPENAI_ORGANIZATION")
+    mongo_initdb_database: str = "mongo_test"
+    mongo_initdb_root_username: str = "mongo"
+    mongo_initdb_root_password: str = "mongo_password"
+    mongo_host: str = "mongodb://127.0.0.1"
+    mongo_port: int = 27017
+    log_level: int = logging.INFO
+    openai_api_key: str = "key"
+    openai_organization: str | None = None
     openai_url: str = "https://api.openai.com/v1/chat/completions"
-
-    class Config:
-        case_sensitive = True
+    # model_config = SettingsConfigDict(case_sensitive=True)
 
 
 config = Settings()  # type: ignore
