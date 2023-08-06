1d261bdfaeb22226b3b10880679d27d09c2cb465
Paul Sanders <psanders1@gmail.com>
2023-08-06 19:41:06 -0400

    More debugging

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -6,10 +6,7 @@ const config: PlaywrightTestConfig = {
     port: 4173
   },
   testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-  use: {
-    trace: 'on'
-  }
+  testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
 
 export default config;
