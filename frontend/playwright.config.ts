c2ed6adc83b0f18dd70a776bac4b99135c1840ed
Paul Sanders <psanders1@gmail.com>
2023-08-07 08:30:41 -0400

    Increate expect timeout

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
-  expect: { timeout: 10000 },
+  expect: { timeout: 15000 },
   use: {
     trace: 'on-first-retry'
   }
